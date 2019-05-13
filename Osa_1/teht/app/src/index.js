60bf3c3a0c087361b5016b62d336ccd0acdfb1f9
Mutkis <jussi.nissi1@gmail.com>
2019-05-13 13:53:42 +0300

    Tehtävä 1.4

--- Osa_1/teht/app/src/index.js
+++ Osa_1/teht/app/src/index.js
@@ -10,7 +10,7 @@ const Header = (props) => {
 const Part = (props) => {
   return (
     <p>
-      {props.part} {props.exer}
+      {props.part.name} {props.part.exercises}
     </p>
   )
 }
@@ -18,9 +18,9 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1} exer={props.exercises1}/>
-      <Part part={props.part2} exer={props.exercises2}/>
-      <Part part={props.part3} exer={props.exercises3}/>
+      <Part part={props.part1}/>
+      <Part part={props.part2}/>
+      <Part part={props.part3}/>
     </div>
   )
 }
@@ -36,18 +36,24 @@ const Total = (props) => {
 const App = () => {
 
   const course = 'Half Stack -sovelluskehitys'
-  const part1 = 'Reactin perusteet'
-  const exercises1 = 10
-  const part2 = 'Tiedonvälitys propseilla'
-  const exercises2 = 7
-  const part3 = 'Komponenttien tila'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Reactin perusteet',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Tiedonvälitys propseilla',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'Komponenttien tila',
+    exercises: 14
+  }
 
   return (
     <>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
-      <Total yht={exercises1 + exercises2 + exercises3} />
+      <Content part1={part1} part2={part2} part3={part3}/>
+      <Total yht={part1.exercises + part2.exercises + part3.exercises} />
     </>
   )
 }
